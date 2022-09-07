90ab135640ae40fd7e783a623b1a599eb214c3fe
Veronica Tamsitt <veronica.tamsitt@gmail.com>
2022-09-07 16:18:38 -0700

    added contour option for META3.2

--- eddymatchup/eddymatchup.py
+++ eddymatchup/eddymatchup.py
@@ -34,11 +34,13 @@ import xarray as xr
 import ftplib
 import getpass
 import datetime
-import glob
+import glob,os
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
 
 def match(lons,lats,datetimes,database='META3.2_allsat',download_database=True,
-          latmin=-90,latmax=-35,lonmin=0,lonmax=360,hourrange=24,radiusrange=1):
+          latmin=-90,latmax=-35,lonmin=0,lonmax=360,hourrange=24,radiusrange=1,use_contour=False):
     
     """
     Inputs:
@@ -46,14 +48,21 @@ def match(lons,lats,datetimes,database='META3.2_allsat',download_database=True,
     lats (array of floats): required; latitude of data to match up with eddy database, -90 to 90
     datetimes (array of python datetime): required; dates and times of data to match up with eddy database
     database (string): optional; which eddy tracking database/algorithm to use, default is META3.2_allsat
-    download_database (True/False): optional, specify whether to download AVISO eddy database file or use existing saved file. 
+    download_database (True/False): optional, specify whether to download AVISO eddy database file or use existing saved file. Default True.
     latmax (float): optional; maximum latitude to consider for matchups
     hourrange (int): optional; maximum time difference (in hours) to consider for eddy matchup, default is +/- 24 hours
-    radiusrange (int or float): optional; maximum distance from eddy centre to include in matchups, such that the max distance considered is radiusrange*(radius from eddy centre to maximum speed). Default value is 1. 
+    radiusrange (int or float): optional; maximum distance from eddy centre to include in matchups, such that the max distance considered is radiusrange*(radius from eddy centre to maximum speed). Default value is 1.
+    use_contour (True/False): optional; flag to use the eddy database contour of maximum speed to identify eddy matchup rather than a circular eddy radius (only works with META3.1 onwards). If True, code ignores radiusrange. Default is False.
     """
     
     #define constant for calculating distance
     dr = 2*np.pi*(6371.136*1000)/360.0
+
+    #check data directory exists, if not create
+    data_dir = os.getcwd()+'/data/'
+    if not os.path.isdir(data_dir):
+        os.mkdir(data_dir)
+    
 
     #download and load eddy database - requires aviso login (FOR NOW JUST USE PRE-DOWNLOADED FILE)
     if database == 'META3.2_allsat':
@@ -75,8 +84,8 @@ def match(lons,lats,datetimes,database='META3.2_allsat',download_database=True,
             ftp.quit()
         
         
-        filename_cyclonic = glob.glob('META3.2_DT_allsat_Cyclonic_long_*.nc')
-        filename_anticyclonic = glob.glob('META3.2_DT_allsat_Anticyclonic_long_*.nc')
+        filename_cyclonic = glob.glob(data_dir+'META3.2_DT_allsat_Cyclonic_long_*.nc')
+        filename_anticyclonic = glob.glob(data_dir+'META3.2_DT_allsat_Anticyclonic_long_*.nc')
         ed_c = xr.load_dataset(filename_cyclonic[0])
         ed_ac = xr.load_dataset(filename_anticyclonic[0])
 
@@ -122,63 +131,159 @@ def match(lons,lats,datetimes,database='META3.2_allsat',download_database=True,
         
         for t in range(len(datetimes)):
             
-            #run for both cyclonic and anticyclonic, then combine
-            
             #find all eddies within +/- dt of the obs time
             eddy_c_day = eddy_c.where((eddy_c.time>datetimes_n[t]-dt) & (eddy_c.time<datetimes_n[t]+dt),drop=True)
             eddy_ac_day = eddy_ac.where((eddy_ac.time>datetimes_n[t]-dt) & (eddy_ac.time<datetimes_n[t]+dt),drop=True)
-   
-            #compute distance from eddy centre lats/lons
-            dy = (eddy_c_day.latitude.values-lats[t])*dr
-            dx = (eddy_c_day.longitude.values-lons_pos[t])*dr*np.cos(np.deg2rad(lats[t]))
-            dists = np.sqrt(dx*dx + dy*dy)
-            eddy_c_day['distance'] = xr.DataArray(dists,dims=['obs'])
-        
-            #repeat for anticyclonic
-            dy = (eddy_ac_day.latitude.values-lats[t])*dr
-            dx = (eddy_ac_day.longitude.values-lons_pos[t])*dr*np.cos(np.deg2rad(lats[t]))
-            dists = np.sqrt(dx*dx + dy*dy)
-            eddy_ac_day['distance'] = xr.DataArray(dists,dims=['obs'])
-        
-            #find if distance less than radiusrange*eddy radius                 
-            if np.any(eddy_c_day.distance < radiusrange*eddy_c_day.speed_radius) or \
-                np.any(eddy_ac_day.distance < radiusrange*eddy_ac_day.speed_radius): 
+            
+            #run for both cyclonic and anticyclonic, then combine
+            
+            #option to use non-circular eddy contour positions
+            if use_contour == True:
                 
-                eddy_c_match = eddy_c_day.where(eddy_c_day.distance<(radiusrange*eddy_c_day.speed_radius),drop=True)
-                eddy_ac_match = eddy_ac_day.where(eddy_ac_day.distance<(radiusrange*eddy_ac_day.speed_radius),drop=True)
-                              
-                #if 1 match save it 
-                if len(eddy_c_match.obs)+len(eddy_ac_match.obs) == 1:
-                    if len(eddy_c_match.obs):
+                #loop through each cyclonic eddy to check contour values
+                for e in range(len(eddy_c_day.time)):
+                    #get contour values
+                    lons_lats_vect = np.column_stack((eddy_c_day.speed_contour_longitude[e,:],
+                                                      eddy_c_day.speed_contour_latitude[e,:]))
+                    
+                    #define polygon using contour values
+                    contour = Polygon(lons_lats_vect)
+                    point = Point(lons_pos[t],lats[t])
+                    
+                    #check if point lies within eddy contour
+                    if point.within(contour) == False:
+                        continue #no eddies are overlapping so if true then can exit loop on this day
+                    
+                    else: 
                         match_type[t] = -1 #-1 is cyclonic
-                        match_track[t] = eddy_c_match.track
-                        match_time[t] = eddy_c_match.time
-                        match_lat[t] = eddy_c_match.latitude
-                        match_lon[t] = eddy_c_match.longitude
-                        match_amp[t] = eddy_c_match.amplitude
-                        match_speed[t] = eddy_c_match.speed_average
-                        match_radius[t] = eddy_c_match.speed_radius
-                        match_age[t] = eddy_c_match.observation_number
-                        match_dist[t] = eddy_c_match.distance
-                    elif len(eddy_ac_match.obs):
-                        match_type[t] = 1 #-1 is cyclonic
-                        match_track[t] = eddy_ac_match.track
-                        match_time[t] = eddy_ac_match.time
-                        match_lat[t] = eddy_ac_match.latitude
-                        match_lon[t] = eddy_ac_match.longitude
-                        match_amp[t] = eddy_ac_match.amplitude
-                        match_speed[t] = eddy_ac_match.speed_average
-                        match_radius[t] = eddy_ac_match.speed_radius
-                        match_age[t] = eddy_ac_match.observation_number
-                        match_dist[t] = eddy_ac_match.distance
-                #else >1 match find closest match and save it
-                else: 
-                    if len(eddy_c_match.obs)>=1:
-                        closest_ind_c = eddy_c_match.distance.argmin()
-                    if len(eddy_ac_match.obs)>=1:
-                        closest_ind_ac = eddy_ac_match.distance.argmin()
-                    if len(closest_ind_c) and len(closest_ind_ac):
-                        if eddy_c_match.distance[closest_ind_c]<eddy_ac_match.distance[closest_ind_ac]:
+                        match_track[t] = eddy_c_day.track[e]
+                        match_time[t] = eddy_c_day.time[e]
+                        match_lat[t] = eddy_c_day.latitude[e]
+                        match_lon[t] = eddy_c_day.longitude[e]
+                        match_amp[t] = eddy_c_day.amplitude[e]
+                        match_speed[t] = eddy_c_day.speed_average[e]
+                        match_radius[t] = eddy_c_day.speed_radius[e]
+                        match_age[t] = eddy_c_day.observation_number[e]
+                        
+                        #compute distance from eddy centre lats/lons
+                        dy = (eddy_c_day.latitude[e].values-lats[t])*dr
+                        dx = (eddy_c_day.longitude[e].values-lons_pos[t])*dr*np.cos(np.deg2rad(lats[t]))
+                        dist = np.sqrt(dx*dx + dy*dy)
+                        match_dist[t] = dist
+                        
+                
+                #if cyclonic match found then don't need to continue to look at cyclonic
+                if match_type[t] == -1:
+                    continue
+                
+                #loop through each anticyclonic eddy to check contour values
+                for e in range(len(eddy_ac_day.time)):
+                                    #get contour values
+                    lons_lats_vect = np.column_stack((eddy_ac_day.speed_contour_longitude[e,:],
+                                                      eddy_ac_day.speed_contour_latitude[e,:]))
+                    
+                    #define polygon using contour values
+                    contour = Polygon(lons_lats_vect)
+                    point = Point(lons_pos[t],lats[t])
+                    
+                    #check if point lies within eddy contour
+                    if point.within(contour) == False:
+                        continue #no eddies are overlapping so if true then can exit loop on this day
+                    
+                    else: 
+                        match_type[t] = 1 #1 is anticyclonic
+                        match_track[t] = eddy_ac_day.track[e]
+                        match_time[t] = eddy_ac_day.time[e]
+                        match_lat[t] = eddy_ac_day.latitude[e]
+                        match_lon[t] = eddy_ac_day.longitude[e]
+                        match_amp[t] = eddy_ac_day.amplitude[e]
+                        match_speed[t] = eddy_ac_day.speed_average[e]
+                        match_radius[t] = eddy_ac_day.speed_radius[e]
+                        match_age[t] = eddy_ac_day.observation_number[e]
+                        
+                        #compute distance from eddy centre lats/lons
+                        dy = (eddy_ac_day.latitude[e].values-lats[t])*dr
+                        dx = (eddy_ac_day.longitude[e].values-lons_pos[t])*dr*np.cos(np.deg2rad(lats[t]))
+                        dist = np.sqrt(dx*dx + dy*dy)
+                        match_dist[t] = dist
+                
+            
+            #else use distance from eddy centre with radius specified by radiusrange
+            else:            
+
+                #compute distance from eddy centre lats/lons
+                dy = (eddy_c_day.latitude.values-lats[t])*dr
+                dx = (eddy_c_day.longitude.values-lons_pos[t])*dr*np.cos(np.deg2rad(lats[t]))
+                dists = np.sqrt(dx*dx + dy*dy)
+                eddy_c_day['distance'] = xr.DataArray(dists,dims=['obs'])
+        
+                #repeat for anticyclonic
+                dy = (eddy_ac_day.latitude.values-lats[t])*dr
+                dx = (eddy_ac_day.longitude.values-lons_pos[t])*dr*np.cos(np.deg2rad(lats[t]))
+                dists = np.sqrt(dx*dx + dy*dy)
+                eddy_ac_day['distance'] = xr.DataArray(dists,dims=['obs'])
+        
+                #find if distance less than radiusrange*eddy radius                 
+                if np.any(eddy_c_day.distance < radiusrange*eddy_c_day.speed_radius) or \
+                    np.any(eddy_ac_day.distance < radiusrange*eddy_ac_day.speed_radius): 
+                
+                    eddy_c_match = eddy_c_day.where(eddy_c_day.distance<(radiusrange*eddy_c_day.speed_radius),drop=True)
+                    eddy_ac_match = eddy_ac_day.where(eddy_ac_day.distance<(radiusrange*eddy_ac_day.speed_radius),drop=True)
+                              
+                    #if 1 match save it 
+                    if len(eddy_c_match.obs)+len(eddy_ac_match.obs) == 1:
+                        if len(eddy_c_match.obs):
+                            match_type[t] = -1 #-1 is cyclonic
+                            match_track[t] = eddy_c_match.track
+                            match_time[t] = eddy_c_match.time
+                            match_lat[t] = eddy_c_match.latitude
+                            match_lon[t] = eddy_c_match.longitude
+                            match_amp[t] = eddy_c_match.amplitude
+                            match_speed[t] = eddy_c_match.speed_average
+                            match_radius[t] = eddy_c_match.speed_radius
+                            match_age[t] = eddy_c_match.observation_number
+                            match_dist[t] = eddy_c_match.distance
+                        elif len(eddy_ac_match.obs):
+                            match_type[t] = 1 #-1 is cyclonic
+                            match_track[t] = eddy_ac_match.track
+                            match_time[t] = eddy_ac_match.time
+                            match_lat[t] = eddy_ac_match.latitude
+                            match_lon[t] = eddy_ac_match.longitude
+                            match_amp[t] = eddy_ac_match.amplitude
+                            match_speed[t] = eddy_ac_match.speed_average
+                            match_radius[t] = eddy_ac_match.speed_radius
+                            match_age[t] = eddy_ac_match.observation_number
+                            match_dist[t] = eddy_ac_match.distance
+                    #else >1 match find closest match and save it
+                    else: 
+                        if len(eddy_c_match.obs)>=1:
+                            closest_ind_c = eddy_c_match.distance.argmin()
+                        if len(eddy_ac_match.obs)>=1:
+                            closest_ind_ac = eddy_ac_match.distance.argmin()
+                        if len(closest_ind_c) and len(closest_ind_ac):
+                            if eddy_c_match.distance[closest_ind_c]<eddy_ac_match.distance[closest_ind_ac]:
+                                match_type[t] = -1
+                                match_track[t] = eddy_c_match.track[closest_ind_c]
+                                match_time[t] = eddy_c_match.time[closest_ind_c]
+                                match_lat[t] = eddy_c_match.latitude[closest_ind_c]
+                                match_lon[t] = eddy_c_match.longitude[closest_ind_c]
+                                match_amp[t] = eddy_c_match.amplitude[closest_ind_c]
+                                match_speed[t] = eddy_c_match.speed_average[closest_ind_c]
+                                match_radius[t] = eddy_c_match.speed_radius[closest_ind_c]
+                                match_age[t] = eddy_c_match.observation_number[closest_ind_c]
+                                match_dist[t] = eddy_c_match.distance[closest_ind_c]
+                            else:
+                                match_type[t] = 1
+                                match_track[t] = eddy_ac_match.track[closest_ind_ac]
+                                match_time[t] = eddy_ac_match.time[closest_ind_ac]
+                                match_lat[t] = eddy_ac_match.latitude[closest_ind_ac]
+                                match_lon[t] = eddy_ac_match.longitude[closest_ind_ac]
+                                match_amp[t] = eddy_ac_match.amplitude[closest_ind_ac]
+                                match_speed[t] = eddy_ac_match.speed_average[closest_ind_ac]
+                                match_radius[t] = eddy_ac_match.speed_radius[closest_ind_ac]
+                                match_age[t] = eddy_ac_match.observation_number[closest_ind_ac]
+                                match_dist[t] = eddy_ac_match.distance[closest_ind_ac]
+                        elif len(closest_ind_c):
                             match_type[t] = -1
                             match_track[t] = eddy_c_match.track[closest_ind_c]
                             match_time[t] = eddy_c_match.time[closest_ind_c]
@@ -189,7 +294,7 @@ def match(lons,lats,datetimes,database='META3.2_allsat',download_database=True,
                             match_radius[t] = eddy_c_match.speed_radius[closest_ind_c]
                             match_age[t] = eddy_c_match.observation_number[closest_ind_c]
                             match_dist[t] = eddy_c_match.distance[closest_ind_c]
-                        else:
+                        elif len(closest_ind_ac):
                             match_type[t] = 1
                             match_track[t] = eddy_ac_match.track[closest_ind_ac]
                             match_time[t] = eddy_ac_match.time[closest_ind_ac]
@@ -199,29 +304,7 @@ def match(lons,lats,datetimes,database='META3.2_allsat',download_database=True,
                             match_speed[t] = eddy_ac_match.speed_average[closest_ind_ac]
                             match_radius[t] = eddy_ac_match.speed_radius[closest_ind_ac]
                             match_age[t] = eddy_ac_match.observation_number[closest_ind_ac]
-                            match_dist[t] = eddy_ac_match.distance[closest_ind_ac]
-                    elif len(closest_ind_c):
-                        match_type[t] = -1
-                        match_track[t] = eddy_c_match.track[closest_ind_c]
-                        match_time[t] = eddy_c_match.time[closest_ind_c]
-                        match_lat[t] = eddy_c_match.latitude[closest_ind_c]
-                        match_lon[t] = eddy_c_match.longitude[closest_ind_c]
-                        match_amp[t] = eddy_c_match.amplitude[closest_ind_c]
-                        match_speed[t] = eddy_c_match.speed_average[closest_ind_c]
-                        match_radius[t] = eddy_c_match.speed_radius[closest_ind_c]
-                        match_age[t] = eddy_c_match.observation_number[closest_ind_c]
-                        match_dist[t] = eddy_c_match.distance[closest_ind_c]
-                    elif len(closest_ind_ac):
-                        match_type[t] = 1
-                        match_track[t] = eddy_ac_match.track[closest_ind_ac]
-                        match_time[t] = eddy_ac_match.time[closest_ind_ac]
-                        match_lat[t] = eddy_ac_match.latitude[closest_ind_ac]
-                        match_lon[t] = eddy_ac_match.longitude[closest_ind_ac]
-                        match_amp[t] = eddy_ac_match.amplitude[closest_ind_ac]
-                        match_speed[t] = eddy_ac_match.speed_average[closest_ind_ac]
-                        match_radius[t] = eddy_ac_match.speed_radius[closest_ind_ac]
-                        match_age[t] = eddy_ac_match.observation_number[closest_ind_ac]
-                        match_dist[t] = eddy_ac_match.distance[closest_ind_ac]                            
+                            match_dist[t] = eddy_ac_match.distance[closest_ind_ac]                            
     
     if database == 'META2.0':
         
